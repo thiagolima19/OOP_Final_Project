63e647fc8c58e078ed27e55575766c3da2649bbb
Patymag29 <patymag29@gmail.com>
2024-04-16 15:49:50 -0600

    Implementation of method Play() and comment adjustments

--- OOP_Final_Project/Program.cs
+++ OOP_Final_Project/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConnectFourGame;
+using System;
 using System.ComponentModel;
 using System.Xml.Linq;
 
@@ -6,24 +7,27 @@ namespace ConnectFourGame
 {
     interface IPlayer //IPlayer is an interface that will work as base for other class that will implement its fields and methods if any
     {
-        int MakeMove(); //this method is incomplete and will be completed inside of the concrete class
+
+        string Name { get; }// Patricia 04/16/24
+        int MakeMove(); //this method is implemented inside of the concrete class
     }
 
-    class HumanPlayer : IPlayer //class to create a humamz player object to allow two humans playing the game
+    class HumanPlayer : IPlayer //class to create a human player object to allow two humans playing the game
     {
-        public string Name { get; } //property Name to hold a player name inputed on the console
+        public string Name { get; } //property Name to hold a player name input on the console
 
         public HumanPlayer(string name) //constructor to initialize Name as the name passed by the user
         {
             Name = name;
         }
 
-        public int MakeMove() // Patricia 04/11/24 - EXCEPTION HANDLING ERRORS and option to leave game
+        public int MakeMove() 
         {
             int col = -1;
             bool validInput = false;
             while (!validInput)
             {
+                // Patricia 04/11/24 - EXCEPTION HANDLING ERRORS and option to leave game
                 try
                 {
                     Console.Write($"\n{Name}, choose a column (1-7): ");
@@ -57,7 +61,6 @@ namespace ConnectFourGame
                     Console.WriteLine($"Unexpected error: Please, try again\n");
                 }
             }
-
             return col;
         }
     }
@@ -66,7 +69,7 @@ namespace ConnectFourGame
     {
         private readonly Random random; //property to generate random numbers inside the range
 
-        public string Name { get; } //property to get the name of the computer player whem a game is played by a human vs a computer player
+        public string Name { get; } //property to get the name of the computer player when a game is played by a human vs a computer player
         public ComputerPlayer() //constructor method to initialize a ComputerPlayer creating a new random object
         {
             random = new Random();
@@ -83,18 +86,17 @@ namespace ConnectFourGame
     {
         private char[,] board; //array to enforce the boundaries of the board 6x7
         private char currentPlayer; //X or O it is used to holds the current player
-        private IPlayer player1; //player 1 and 2 is an Iplayer interface object
+        private IPlayer player1; //player 1 and 2 is an IPlayer interface object
         private IPlayer player2; // may be a human or the computer
 
         public ConnectFourGame(IPlayer player1, IPlayer player2) // Patricia 04/07/24
         {
-            //todo
-            //properties initializer constructor
+        //properties initializer constructor
             board = new char[6, 7];
-            currentPlayer = 'X';
+            currentPlayer = 'X'; //X or O it is used to holds the current player, but starts using this value
             InitializeBoard();
-            this.player1 = player1;
-            this.player2 = player2;
+            this.player1 = player1; //player 1 and 2 is an Iplayer interface object
+            this.player2 = player2; // may be a human or the computer player
         }
 
         private void InitializeBoard() // Patricia 04/07/24
@@ -108,26 +110,29 @@ namespace ConnectFourGame
             }
         }
 
+        // this method will print the empty string to hold the place for a piece and displays values over the game 
         public void PrintBoard() // Patricia 04/07/24
-        { // this method will print the empty string to hold the place for a piece and displays values over the game 
+        { 
             for (int row = 0; row < 6; row++)
             {
                 for (int col = 0; col < 7; col++)
-                {                   
+                {
                     Console.Write($" | {board[row, col]}");
                 }
                 Console.WriteLine(" |");
             }
-                 Console.WriteLine(" -----------------------------");
-                 Console.WriteLine("   1   2   3   4   5   6   7  ");               
+            Console.WriteLine(" -----------------------------");
+            Console.WriteLine("   1   2   3   4   5   6   7  ");
         }
 
-        public bool IsValidMove(int col) //this boolean method will check if there is a empty string on the column select, and if it is a valid column inside the board
-        {
+        //this boolean method will check if there is a empty string on the column select, and if it is a valid column inside the board
+        public bool IsValidMove(int col)
+        { 
             return board[0, col] == ' ';
         }
 
-        public void MakeMove(int col) // this method will drop the piece as if gravity was pulling it to the floor. So, it will start placing the pieces from the botton to the top on the column.
+        // this method will drop the piece as if gravity was pulling it to the floor. So, it will start placing the pieces from the bottom to the top on the column
+        public void MakeMove(int col) 
         {
             for (int row = 5; row >= 0; row--)
             {
@@ -139,7 +144,8 @@ namespace ConnectFourGame
             }
         }
 
-        public string CheckWinner() //this method will check a winner using the methods CheckHorizontal, CheckVertical, CheckDiagonal and CheckAntiDiagonal pieces position.
+        //this method will check a winner using the methods CheckHorizontal, CheckVertical, CheckDiagonal and CheckAntiDiagonal pieces position.
+        public string CheckWinner() 
         {
             char playerSymbol = currentPlayer == 'X' ? 'X' : 'O'; //this line will check for the current player and will change it to the another one
 
@@ -160,8 +166,8 @@ namespace ConnectFourGame
             // Check horizontally
             for (int row = 0; row < 6; row++) //outer loop goes from index 0 to 6 in the rows
             {
-                for (int col = 0; col < 4; col++) //inner loop will check evrey 4 places up the max number of columns 
-                {   //it will check if on each place in the board will have the same symblo/char from the same player than will return a winner if any
+                for (int col = 0; col < 4; col++) //inner loop will check every 4 places up the max number of columns 
+                {   //it will check if on each place in the board will have the same symbol/char from the same player than will return a winner if any
                     if (board[row, col] == playerSymbol && board[row, col + 1] == playerSymbol && board[row, col + 2] == playerSymbol && board[row, col + 3] == playerSymbol)
                         return true;
                 }
@@ -174,8 +180,8 @@ namespace ConnectFourGame
             // Check vertically
             for (int col = 0; col < 7; col++)//outer loop goes from index 0 to 6 in the columns 
             {
-                for (int row = 0; row < 3; row++)  //inner loop will check evrey 4 places up the max number of rows
-                {   //it will check if on each place in the board will have the same symblo/char from the same player than will return a winner if any
+                for (int row = 0; row < 3; row++)  //inner loop will check every 4 places up the max number of rows
+                {   //it will check if on each place in the board will have the same symbol/char from the same player than will return a winner if any
                     if (board[row, col] == playerSymbol && board[row + 1, col] == playerSymbol && board[row + 2, col] == playerSymbol && board[row + 3, col] == playerSymbol)
                         return true;
                 }
@@ -189,7 +195,7 @@ namespace ConnectFourGame
             for (int row = 0; row < 3; row++)//outer loop goes from index 0 to 2 in the rows, because the row only contains true diagonals from the index 0 to 2 that fits 4 pieces
             {
                 for (int col = 0; col < 4; col++) //inner loop goes from index 0 to 3 in the columns, because the columns only contains true diagonals from the index 0 to 3 that fits 4 pieces
-                {   //it will check if on each place in the board will have the same symblo/char from the same player than will return a winner if any
+                {   //it will check if on each place in the board will have the same symbol/char from the same player than will return a winner if any
                     if (board[row, col] == playerSymbol && board[row + 1, col + 1] == playerSymbol && board[row + 2, col + 2] == playerSymbol && board[row + 3, col + 3] == playerSymbol)
                         return true;
                 }
@@ -203,7 +209,7 @@ namespace ConnectFourGame
             for (int row = 0; row < 3; row++) //outer loop goes from index 0 to 2 in the rows, because the row only contains true anti-diagonals from the index 0 to 2 that fits 4 pieces
             {
                 for (int col = 3; col < 7; col++)//inner loop goes from index 3 to 6 in the columns, because the columns only contains true anti-diagonals from the index 3 to 6 that fits 4 pieces
-                {   //it will check if on each place in the board will have the same symblo/char from the same player than will return a winner if any
+                {   //it will check if on each place in the board will have the same symbol/char from the same player than will return a winner if any
                     if (board[row, col] == playerSymbol && board[row + 1, col - 1] == playerSymbol && board[row + 2, col - 2] == playerSymbol && board[row + 3, col - 3] == playerSymbol)
                         return true;
                 }
@@ -211,7 +217,8 @@ namespace ConnectFourGame
             return false;
         }
 
-        public bool CheckDraw() //this method will check for a draw
+        //this method will check if the game gets a draw
+        public bool CheckDraw()  
         {
             for (int col = 0; col < 7; col++) //this for loop will go through the board checking if the move is valid or not
             {
@@ -221,11 +228,67 @@ namespace ConnectFourGame
             return true;
         }
 
-        public void Play() // not finished
+        // this method allows the player to choose to play again or quit
+        private bool PromptPlayAgain() // Patricia 04/16/24
         {
-            //todo
-            //implement the Play method
-            PrintBoard(); //Patricia 04/07/24
+            Console.Write("\nDo you want to play again? (yes/no): ");
+            string input = Console.ReadLine().Trim().ToLower();
+            return input == "yes" || input == "y";
+        }
+
+
+        public void Play() //Patricia 04/16/24
+        {
+
+            while (true)
+            {
+                PrintBoard();
+
+                int col;
+                if (currentPlayer == 'X')
+                    col = player1.MakeMove();
+                else
+                    col = player2.MakeMove();
+
+                if (col >= 0 && col < 7 && IsValidMove(col))
+                {
+                    MakeMove(col);
+                    string winner = CheckWinner();
+                    if (!string.IsNullOrEmpty(winner))
+                    {
+                        PrintBoard();
+                        Console.WriteLine($"Congratulations!!!\nPlayer {winner} wins!");
+
+                        if (PromptPlayAgain())
+                        {
+                            InitializeBoard();
+                            currentPlayer = 'X';
+                            continue;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    else if (CheckDraw())
+                    {
+                        PrintBoard();
+                        Console.WriteLine("Sorry!!!\nIt's a draw!");
+
+                        if (PromptPlayAgain())
+                        {
+                            InitializeBoard();
+                            currentPlayer = 'X';
+                            continue;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                }                
+            }
         }
     }
 
@@ -241,5 +304,7 @@ namespace ConnectFourGame
             ConnectFourGame game = new ConnectFourGame(humanPlayer1, humanPlayer2);
             game.Play();
         }
-    }
+    }     
 }
+    
+
